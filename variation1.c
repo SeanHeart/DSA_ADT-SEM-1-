d7a10b1bf06ed751153ff0647446912e22182d35
SeanHeart <seanheartgarcines@gmail.com>
2025-09-02 21:13:38 +0800

    updated version

--- variation1.c
+++ variation1.c
@@ -57,10 +57,24 @@ List insertSorted(List L, int data){
     if(L.count == MAX){
         printf("Array is full!\n");
     }
-    
-    for(int i=0; i<L.count; i++){
 
+    int position = 0;
+    
+    //find the position as to where the data should go
+    while(position < L.count && L.elem[position] < data){
+        position++;
     }
+
+    //shift elements to the right
+    for(int i=L.count; i>position; i--){
+        L.elem[i] = L.elem[i-1];
+    }
+
+    //insert new element
+    L.elem[position] = data;
+    L.count++;
+
+    return L;
 }
 
 void display(List L){
@@ -105,10 +119,25 @@ int main(){
     printf("Locate\n");
     printf("Search for the postion of 670\n");
     printf("%d\n", locate(L, 670));
+
+    printf("\n");
+
+    printf("Insert Sorted\n");
+    printf("Before:\n");
+    L = insertPos(L, 1, 0);
+    L = insertPos(L, 3, 1);
+    L = insertPos(L, 5, 2);
+    L = insertPos(L, 10, 3);
+
+    L = insertSorted(L, 8);
+
+    printf("After:\n");
+    display(L);
+    
     
  
 
     return 0;
 }
 
-//goods na
+//Update [09-02-2025]: goods na i think
