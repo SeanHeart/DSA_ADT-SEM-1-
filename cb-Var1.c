0a9d9074e09f2e96caad794bfdd16dabe715008d
SeanHeart <seanheartgarcines@gmail.com>
2026-01-05 22:18:34 +0800

    DONE

--- cb-Var1.c
+++ cb-Var1.c
@@ -61,11 +61,46 @@ void insertLast(int* L, VHeap* V, int elem){
     }
 }
 
-//void insertPos(int* L, VHeap* V, int elem);
+void insertPos(int* L, VHeap* V, int elem){
+    int newCell = allocSpace(V);
+    if(newCell == -1){
+        printf("Heap is full!\n");
+        return;
+    }
 
-// void insertSorted(int* L, VHeap* V, int elem){
+    V->H[newCell].elem = elem;
 
-// }
+    //if list is empty or only has one element, insert at beginning
+    if(*L == -1 || V->H[*L].next == -1){
+        V->H[newCell].next = *L;
+        *L = newCell;
+        return;
+    }
+
+    //insert after first element
+    int *trav = &V->H[*L].next;
+    V->H[newCell].next = *trav;;
+    *trav = newCell;
+
+}
+
+void insertSorted(int* L, VHeap* V, int elem){
+    int newCell = allocSpace(V);
+    if(newCell == -1){
+        printf("Heap is full!\n");
+        return;
+    }
+
+    V->H[newCell].elem = elem;
+
+    int *trav = L;
+    while(*trav != -1 && V->H[*trav].elem < elem){
+        trav = &V->H[*trav].next;
+    }
+
+    V->H[newCell].next = *trav;
+    *trav = newCell;
+}
 
 void delete(int* L, VHeap* V, int elem){
     int *trav, temp;
@@ -80,12 +115,26 @@ void delete(int* L, VHeap* V, int elem){
     }
 }
 
-// void deleteAllOccurrence(int* L, VHeap* V, int elem);
+void deleteAllOccurrence(int* L, VHeap* V, int elem){
+    int *trav, temp;
+    trav = L;
+    while(*trav != -1){
+        if(V->H[*trav].elem == elem){
+            temp = *trav;
+            *trav = V->H[*trav].next;
+            deallocSpace(V, temp);
+        }else{
+            trav = &V->H[*trav].next;
+        }
+    }
+}
 
 void display(int L, VHeap V){
     for(; L != -1; L = V.H[L].next){
         printf("%d ", V.H[L].elem);
     }
+
+    printf("\n");
 
 }
 
@@ -102,20 +151,26 @@ int main()
     insertFirst(&L, &VH, 30);
     insertFirst(&L, &VH, 40);
 
-    //print the list
-    printf("List elements: \t");
+    printf("Initial List: \n");
     display(L, VH);
 
-    //deleted
-    printf("\nDeleted elements: ");
-    delete(&L, &VH, 30);
+    printf("\nAfter Delete 20: \n");
+    delete(&L, &VH, 20);
     display(L, VH);
 
-    
+    printf("\nAfter insertSorted 25: \n");
+    insertSorted(&L, &VH, 25);
+    display(L, VH);
 
-    printf("\n");
+    printf("\nAfter deleteAllOccurrence 30: \n");
+    deleteAllOccurrence(&L, &VH, 30);
+    display(L, VH);
+
+    printf("\nAfter insertPos:\n");
+    insertPos(&L, &VH, 97);
+    display(L, VH);
 
     return 0;
 }
 
-//Status: Lacking insertSorted() and deleteAllOccurrence() ahhhmam
+//DONE8
